26ec306e23a19625642a346533e7f8c0c6272c09
Emre Ozdemir <emre@Emres-MacBook-Pro-2.local>
2017-02-27 15:22:51 +0300

    listed

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -3,12 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'simple-form',
   template: `<div>
-hello
+  Mesajlar;
+  <ul>
+  <li *ngFor="let message of messages">{{message}}</li>
+  </ul>
   </div>
   `,
   styles: []
 })
 export class SimpleFormComponent implements OnInit {
+
+  messages = ["angular 2", "öğreniyorum"];
 
   constructor() {
 
