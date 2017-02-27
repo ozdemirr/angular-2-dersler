bfbc8b64b1f289a26021879019f68c0e5e6174da
Emre Ozdemir <emre@Emres-MacBook-Pro-2.local>
2017-02-27 16:11:30 +0300

    done

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -3,15 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'simple-form',
   template: `<div>
-hello
+  <input [(ngModel)]="message">
+  <h1>Mesajınız : {{message}}</h1>
   </div>
   `,
   styles: []
 })
 export class SimpleFormComponent implements OnInit {
 
-  constructor() {
+  message = "merhaba";
 
+  constructor() {
+    setInterval(() => this.message = Math.random().toString(), 1000);
   }
 
   ngOnInit() {
