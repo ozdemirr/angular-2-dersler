34c083a6307a612eaabb6ae9e8504b939e450abe
Emre Ozdemir <emre@Emres-MacBook-Pro-2.local>
2017-02-28 16:11:48 +0300

    listed

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -3,12 +3,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'simple-form',
   template: `<div>
-hello
+  <ul>
+  <p>Üyeler</p>
+    <li *ngFor="let user of users"
+    (click)="selectUser(user)">
+      {{user.name}}
+    </li>
+  </ul>
+  <div *ngIf="activeUser">
+  <p>aktif üye</p>
+  <h1>İsim:{{activeUser.name}}</h1>
+  <h2>Yaş:{{activeUser.age}}</h2>
+  </div>
   </div>
   `,
   styles: []
 })
 export class SimpleFormComponent implements OnInit {
+
+  activeUser;
+
+  users = [
+    {
+      name:"emre",
+      age:25
+    },
+    {
+      name:"melis",
+      age : 24
+    }
+  ];
+
+  selectUser(user){
+    this.activeUser = user;
+  }
 
   constructor() {
 
